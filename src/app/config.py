2039868f9f6133dc21924091d1cbca8729cf7d9d
Colin Swaney <colinswaney@princeton.edu>
2024-04-29 11:47:20 -0400

    Create default config values

--- src/app/config.py
+++ src/app/config.py
@@ -1,74 +1,51 @@
 import os
 import configparser
+from copy import deepcopy
+
+
+DEFAULT_HOME = os.path.expanduser("~/.blackfish")
+DEFAULT_CACHE = os.path.expanduser("~/.blackfish/cache")
 
 
 class BlackfishConfig:
     """Blackfish app configuration.
 
+    Create a configuration object based on a provided profile. Environment variables
+    take precedence over profile values and default values; profile values are
+    preferred over defaults.
+
     # Arguments
-    - profile: str. Set `profile="base"` to generate init config variables and
-        `profile=None` to populate all other args. Provide any other value to pull
-        args from a config file.
+    - profile: str. Leave empty to generate baseline config variables.
     """
 
-    def __init__(self, profile="default", user=None, host=None, cache=None):
-        if profile == "base":
-            self.BLACKFISH_HOME = os.getenv(
-                "BLACKFISH_HOME", os.path.expanduser("~/.blackfish")
-            )
-            self.BLACKFISH_ENV = os.getenv(
-                "BLACKFISH_ENV", "local"
-            )  # "local", "head" or "compute"
+    def __init__(self, profile=None):
+        if profile is None:
+            self.BLACKFISH_USER = os.getenv("BLACKFISH_USER", None)
+            self.BLACKFISH_HOST = os.getenv("BLACKFISH_HOST", None)
+            self.BLACKFISH_HOME = os.getenv("BLACKFISH_HOME", DEFAULT_HOME)
+            self.BLACKFISH_CACHE = os.getenv("BLACKFISH_CACHE", DEFAULT_CACHE)
             self.BLACKFISH_DEBUG = os.getenv("BLACKFISH_DEBUG", True)
-        elif profile is None:
-            self.BLACKFISH_HOME = os.getenv(
-                "BLACKFISH_HOME", os.path.expanduser("~/.blackfish")
-            )
-            self.BLACKFISH_USER = user
-            self.BLACKFISH_HOST = host
-            self.BLACKFISH_CACHE = cache
-            self.BLACKFISH_REMOTE = os.getenv(
-                "BLACKFISH_REMOTE", f"/home/{self.BLACKFISH_USER}/.blackfish"
-            )
-            self.BLACKFISH_ENV = os.getenv(
-                "BLACKFISH_ENV", "local"
-            )  # "local", "head" or "compute"
-            self.BLACKFISH_DEBUG = os.getenv("BLACKFISH_DEBUG", True)
-            self.APPTAINER_CACHE = f"/scratch/gpfs/{self.BLACKFISH_USER}"
-            self.APPTAINER_TMPDIR = "/tmp"
         else:
-            self.BLACKFISH_HOME = os.getenv(
-                "BLACKFISH_HOME", os.path.expanduser("~/.blackfish")
-            )
-
             config = configparser.ConfigParser()
-            config.read(f"{self.BLACKFISH_HOME}/config")
-
-            self.BLACKFISH_DEBUG = os.getenv("BLACKFISH_DEBUG", True)
-            self.BLACKFISH_ENV = os.getenv(
-                "BLACKFISH_ENV", "local"
-            )  # "local", "head" or "compute"
-            self.APPTAINER_TMPDIR = "/tmp"
+            config.read(os.path.expanduser("~/.blackfish/config"))
             if profile in config:
                 self.BLACKFISH_USER = os.getenv(
-                    "BLACKFISH_USER", config[profile]["user"]
+                    "BLACKFISH_USER", config[profile].get("user", None)
                 )
                 self.BLACKFISH_HOST = os.getenv(
-                    "BLACKFISH_HOST", config[profile]["host"]
+                    "BLACKFISH_HOST", config[profile].get("host", None)
+                )
+                self.BLACKFISH_HOME = os.getenv(
+                    "BLACKFISH_HOME", config[profile].get("home", DEFAULT_HOME)
                 )
                 self.BLACKFISH_CACHE = os.getenv(
-                    "BLACKFISH_CACHE", config[profile]["cache"]
-                )  # TODO - Make default /scratch/gpfs/BLACKFISH.
-                self.BLACKFISH_REMOTE = os.getenv(
-                    "BLACKFISH_REMOTE", f"/home/{self.BLACKFISH_USER}/.blackfish"
+                    "BLACKFISH_CACHE", config[profile].get("cache", DEFAULT_CACHE)
                 )
-                self.APPTAINER_CACHE = f"/scratch/gpfs/{self.BLACKFISH_USER}"
+                self.BLACKFISH_DEBUG = os.getenv(
+                    "BLACKFISH_DEBUG", config[profile].get("debug", True)
+                )
             else:
-                self.BLACKFISH_USER = None
-                self.BLACKFISH_HOST = None
-                self.BLACKFISH_CACHE = None
-                self.BLACKFISH_REMOTE = None
-                self.APPTAINER_CACHE = None
+                raise Exception(f"Profile {profile} does not exist. Available sections are: {config.sections()}")
 
     def __str__(self) -> str:
         return str(self.__dict__)
@@ -76,7 +53,10 @@ class BlackfishConfig:
     def __repr__(self) -> str:
         inner = ", ".join([f"{k}: {v}" for k, v in self.__dict__.items()])
         return f"BlackfishConfig({inner})"
+    
+    def as_dict(self) -> dict:
+        return deepcopy(self.__dict__)
 
 
-base_config = BlackfishConfig(profile="base")
+base_config = BlackfishConfig()
 default_config = BlackfishConfig(profile="default")

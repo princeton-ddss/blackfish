695291db29ea3d829af188519dd80d3fd617ec1c
fjying <jf3375@princeton.edu>
2024-09-11 14:25:35 -0400

    #39: Update local slurm script

--- src/app/templates/speech_recognition_local.sh
+++ src/app/templates/speech_recognition_local.sh
@@ -1,14 +1,28 @@
 {% extends "base_local.sh" %}
 {% block command %}
 {%- if container_config.provider == "docker" %}
-docker pull fjying/audiototextapi:arm64_hf
+{# The service API port is set as 8085 in the API Request#}
+{# docker pull fjying/audiototextapi:arm64_hf #}
 docker run -d \
   {{ ' --gpus all' if job_config.gres else '' }} \
-  -p $port:$port \
-  -e MODEL_NAME={{container_config['model_name']}}\
-  -e MODEL_SIZE={{container_config['model_size']}}\
-  -e MODEL_FOLDER={{job_config.model_dir}}\
-  -e INPUT_FOLDER={{container_config["input_dir"]}}\
-  -v {{container_config["input_dir"]}}:/app/files \
-  --name speech_recognition_api_container
+  -p {{container_config["port"]}}:{{container_config["port"]}} \
+  {%- if 'revision' in container_config %}
+  -e REVISION={{container_config['revision']}}\
+  {%- endif %}
+  -e SPEECH_RECOGNITION_PORT={{container_config["port"]}}\
+  -v "{{container_config["input_dir"]}}":/app/files \
+  -e MODEL_DIR="/app/files/models/Whisper_hf/models--openai--whisper-tiny"\
+  -e INPUT_DIR="/app/files/data"\
+  --name speech_recognition \
   fjying/audiototextapi:arm64_hf
+{%- elif container_config.provider == 'apptainer' %}
+apptainer run {{ ' --nv' if job_config.gres > 0 else '' }} \
+  {%- if 'revision' in container_config %}
+  --env="REVISION={{container_config['revision']}}" \
+  {%- endif %}
+  --env="SPEECH_RECOGNITION_PORT={{container_config["port"]}}" \
+  --env="MODEL_DIR="{{job_config.model_dir}}"" \
+  --env="INPUT_DIR="{{container_config['input_dir']}}"" \
+  "{{ job_config.cache_dir }}/images/audiototextapi_amd64_hf.sif"
+{%- endif %}
+{%- endblock %}

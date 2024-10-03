047a84af7459e1a053808ddf981c2d54966504fc
Colin Swaney <colinswaney@princeton.edu>
2024-10-03 09:33:25 -0400

    Hot fix to `speech_recognition_local.sh`

--- src/app/templates/speech_recognition_local.sh
+++ src/app/templates/speech_recognition_local.sh
@@ -1,31 +1,29 @@
 {% extends "base_local.sh" %}
 {% block command %}
 {%- if container_config.provider == "docker" %}
-{# The service API port is set as 8085 in the API Request#}
-{# docker pull fjying/audiototextapi:arm64_hf #}
-docker run -d \
-  {{ ' --gpus all' if job_config.gres else '' }} \
-  -p {{container_config["port"]}}:{{container_config["port"]}} \
-  -v "{{container_config["input_dir"]}}":"/data/audio" \
-  -v "{{container_config["model_dir"]}}":"/data/model" \
-  --name speech_recognition \
-  fjying/audiototextapi:arm64_hf\
-  --model_dir "/data/model" \
-  --model_id {{container_config['model_id']}}\
+docker run -d {{ ' --gpus all' if job_config.gres else '' }} \
+  -p {{container_config["port"]}}:{{ container_config.port }} \
+  -v {{container_config["input_dir"]}}:/data/audio \
+  -v {{container_config["model_dir"]}}:/data/models \
+  --name {{ name }} \
+  ghcr.io/princeton-ddss/speech-recognition-inference:latest \
+  --model_dir /data/models \
+  --model_id {{container_config['model_id']}} \
   {%- if 'revision' in container_config %}
-  --revision {{container_config['revision']}}\
+  --revision {{container_config['revision']}} \
   {%- endif %}
-  --port {{container_config["port"]}}
+  --port {{ container_config.port }}
 {%- elif container_config.provider == 'apptainer' %}
 apptainer run {{ ' --nv' if job_config.gres > 0 else '' }} \
-  --bind "{{container_config["input_dir"]}}":"/data/audio" \
-  --bind "{{container_config["model_dir"]}}":"/data/model" \
-  {{ job_config.cache_dir }}/images/audiototextapi_amd64_hf.sif \
-  --model_dir "/data/model" \
-  --model_id {{container_config['model_id']}}\
+  --bind {{container_config["input_dir"]}}:/data/audio \
+  --bind {{container_config["model_dir"]}}:/data/models \
+  {{ job_config.cache_dir }}/images/speech-recognition-inference_latest.sif \
+  {{ name }}
+  --model_dir /data/models \
+  --model_id {{ container_config['model_id'] }} \
   {%- if 'revision' in container_config %}
   --revision {{container_config['revision']}}\
   {%- endif %}
-  --port $port
+  --port {{ container_config.port }}
 {%- endif %}
 {%- endblock %}

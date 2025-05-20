5e8fdb28104d5803472cf7f01630d03f7805e89e
Colin Swaney <colinswaney@gmail.com>
2025-05-20 18:43:06 -0400

    Rollback vLLM to `v0.8.4`
    Rollback required due to errors encountered running certain models, e.g., `DeepSeek-V2-Lite`. See https://github.com/vllm-project/vllm/issues/16908.

--- src/app/templates/text_generation_local.sh
+++ src/app/templates/text_generation_local.sh
@@ -5,17 +5,19 @@ docker run -d {{ '--runtime nvidia --gpus all' if job_config.gres else '' }} \
   -p {{ container_config.port }}:{{ container_config.port }} \
   -v {{ container_config.model_dir }}:/data \
   --name {{ name }} \
-  vllm/vllm-openai:v0.8.5 \
+  vllm/vllm-openai:v0.8.4 \
   --model /data/snapshots/{{ container_config['revision'] }} \
   --port {{ container_config.port }} \
-  --revision {{ container_config.revision }}
+  --revision {{ container_config.revision }} \
+  --trust-remote-code \
 {%- elif provider == 'apptainer' %}
 apptainer run {{ ' --nv' if job_config.gres else '' }} \
   --bind {{ container_config.model_dir }}:/data \
-  {{ profile.cache_dir }}/images/vllm-openai_v0.8.5.sif \
+  {{ profile.cache_dir }}/images/vllm-openai_v0.8.4.sif \
   --model /data/snapshots/{{ container_config['revision'] }} \
   --port {{ container_config.port }} \
-  --revision {{ container_config.revision }}
+  --revision {{ container_config.revision }} \
+  --trust-remote-code \
 {%- endif %}
   {{ container_config.launch_kwargs }}
 {%- endblock %}
--- src/app/templates/text_generation_slurm.sh
+++ src/app/templates/text_generation_slurm.sh
@@ -2,10 +2,11 @@
 {% block command %}
 apptainer run {{ '--nv' if job_config.gres else '' }} \
   --bind {{ container_config.model_dir }}:/data \
-  {{ profile.cache_dir }}/images/vllm-openai_v0.8.5.sif \
+  {{ profile.cache_dir }}/images/vllm-openai_v0.8.4.sif \
   --model /data/snapshots/{{ container_config['revision'] }} \
-  --trust-remote-code \
   --port $port \
-  --revision {{ container_config.revision }}
+  --revision {{ container_config.revision }} \
+  --trust-remote-code \
+  --tensor-parallel-size {{ job_config.gres }} \
   {{ container_config.launch_kwargs }}
 {%- endblock %}

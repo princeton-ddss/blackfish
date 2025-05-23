0799857b02257c5cd59435046735ac532293f659
Colin Swaney <colinswaney@gmail.com>
2025-05-23 13:10:52 -0400

    Fix local Slurm job submission

--- src/app/templates/text_generation_local.sh
+++ src/app/templates/text_generation_local.sh
@@ -11,9 +11,10 @@ docker run -d {{ '--runtime nvidia --gpus all' if job_config.gres else '' }} \
   --revision {{ container_config.revision }} \
   --trust-remote-code \
 {%- elif provider == 'apptainer' %}
-apptainer run {{ ' --nv' if job_config.gres else '' }} \
+apptainer instance run {{ ' --nv' if job_config.gres > 0 else '' }} \
   --bind {{ container_config.model_dir }}:/data \
   {{ profile.cache_dir }}/images/vllm-openai_v0.8.4.sif \
+  {{ name }} \
   --model /data/snapshots/{{ container_config['revision'] }} \
   --port {{ container_config.port }} \
   --revision {{ container_config.revision }} \

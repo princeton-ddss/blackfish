44477b01301d8ef169e284ea38f0a9c1376cf4b7
Colin Swaney <colinswaney@princeton.edu>
2025-06-26 09:10:33 -0400

    Create transcription batch job templates

---
+++ src/app/templates/jobs/speech_recognition_slurm.sh
@@ -0,0 +1,17 @@
+{% extends "base_slurm.sh" %}
+{% block command %}
+
+XDG_RUNTIME_DIR=""
+
+apptainer run {{ ' --nv' if job_config.gres > 0 else '' }} \
+  --bind {{ mount }}:/data/audio \
+  --bind {{ container_config.model_dir }}:/data/models \
+  {{ profile.cache_dir }}/images/speech-recognition-inference_0.2.1.sif \
+  pipeline \
+  /data/audio \
+  --model-dir /data/models \
+  --model-id {{ model }} \
+  --revision {{ container_config.revision }} \
+  --job-id {{ uuid }} \
+  {{ " ".join(container_config.kwargs) }}
+{%- endblock %}

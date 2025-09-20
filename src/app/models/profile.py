2c6df1da8057a493c15037ca5192336ecbc6a1a7
Colin Swaney <colinswaney@gmail.com>
2025-09-20 11:07:07 -0400

    Fix linting errors

--- src/app/models/profile.py
+++ src/app/models/profile.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Union, Optional
+from typing import Union
 from configparser import ConfigParser
 import os
 

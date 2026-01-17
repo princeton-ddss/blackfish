8157140443d2df50bf11447b48e34da005575b4f
Colin Swaney <colinswaney@gmail.com>
2026-01-17 18:16:14 -0500

    Fix button bg

--- web/src/components/NavbarLight.jsx
+++ web/src/components/NavbarLight.jsx
@@ -91,7 +91,7 @@ function NavbarLight({ task }) {
                     href="https://github.com/princeton-ddss/blackfish/issues"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="inline-flex items-center w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:col-start-2"
+                    className="inline-flex items-center w-full justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 sm:col-start-2"
                   >
                     View on GitHub
                   </a>

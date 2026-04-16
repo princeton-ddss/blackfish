682150e0ea4b34c8fae656e7f5c3dad66fc4cbf7
Colin Swaney <colinswaney@princeton.edu>
2026-04-16 00:44:04 -0400

    fix(web): remove unimplemented chat history button
    See #200 for the feature issue.
    
    Closes #262.

--- web/src/routes/text-generation/text-generation.jsx
+++ web/src/routes/text-generation/text-generation.jsx
@@ -11,7 +11,6 @@ import {
 } from "@heroicons/react/20/solid";
 import {
   CodeBracketIcon,
-  ClockIcon,
 } from "@heroicons/react/24/outline";
 
 import { Page } from "@/components/Page";
@@ -240,14 +239,6 @@ export default function TextGenerationPage() {
                   <CodeBracketIcon className="h-5 w-5" />
                   <span className="absolute top-full left-1/2 -translate-x-1/2 mt-0.5 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 pointer-events-none">Code</span>
                 </button>
-                <button
-                  type="button"
-                  className="group relative p-2 mr-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-gray-700 rounded-md"
-                  aria-label="History"
-                >
-                  <ClockIcon className="h-5 w-5" />
-                  <span className="absolute top-full left-1/2 -translate-x-1/2 mt-0.5 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 pointer-events-none">History</span>
-                </button>
                 <ModeSelect selectedMode={mode} setSelectedMode={setMode} />
               </div>
             }
@@ -266,14 +257,6 @@ export default function TextGenerationPage() {
                 >
                   <CodeBracketIcon className="h-5 w-5" />
                   <span className="absolute top-full left-1/2 -translate-x-1/2 mt-0.5 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 pointer-events-none">Code</span>
-                </button>
-                <button
-                  type="button"
-                  className="group relative p-2 mr-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-gray-700 rounded-md"
-                  aria-label="History"
-                >
-                  <ClockIcon className="h-5 w-5" />
-                  <span className="absolute top-full left-1/2 -translate-x-1/2 mt-0.5 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 pointer-events-none">History</span>
                 </button>
                 <ModeSelect selectedMode={mode} setSelectedMode={setMode} />
               </div>
